ff9893cb92a90ecd0af20cb4d3f14f7ad8956e8b
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-05 15:03:34 +0530

    editted rotate array

--- Arrays & Hashing/Rotate Array By D places.js
+++ Arrays & Hashing/Rotate Array By D places.js
@@ -1,20 +1,25 @@
 function rotateByd(arr,d){
+    let len = arr.length;
 
-    
+    let index = d%len;
+
+    if(index === 0) return arr;
+
     let temp =[];
-    for(let i = 0;i<d;i++){
+
+    for(let i = 0;i<index;i++){
         temp[i] = arr[i];
     }
 
-    for(let i = d;i<arr.length;i++){
-        arr[i-d] = arr[i];
+    for(let i = index;i<len;i++){
+        arr[i-index] = arr[i];
     }
 
-    for(let i=0;i<d;i++){
-        arr[arr.length-d+i] = temp[i];
+    for(let i=0;i<index;i++){
+        arr[len-index+i] = temp[i];
     }
 
     return arr;
 }
 
-console.log(rotateByd([1,0],2));
+console.log(rotateByd([1,0],3));
